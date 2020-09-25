b36f660daf58e638778afa2e1822dae02a6a26d1
Chongyu <javawcy@outlook.com>
2020-09-25 18:19:52 +0800

    fix: change refresh

--- server-demo1/src/main/java/dev/lowdad/cloud/serverdemo1/config/Oauth2ResourceConfig.java
+++ server-demo1/src/main/java/dev/lowdad/cloud/serverdemo1/config/Oauth2ResourceConfig.java
@@ -2,6 +2,7 @@ package dev.lowdad.cloud.serverdemo1.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -40,6 +41,7 @@ public class Oauth2ResourceConfig extends ResourceServerConfigurerAdapter {
     }
 
     @Bean
+    @RefreshScope
     public JwtAccessTokenConverter jwtTokenEnhancer(){
         JwtAccessTokenConverter jwtTokenEnhancer = new JwtAccessTokenConverter();
         jwtTokenEnhancer.setSigningKey(jwtSignConfiguration.getSignKey());
