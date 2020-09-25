e82431f8e66d6c5f1ae1ae51fb1a965f2b6e994e
Chongyu <javawcy@outlook.com>
2020-09-25 16:42:18 +0800

    fix: change token store to jwt

--- server-demo1/src/main/java/dev/lowdad/cloud/serverdemo1/config/Oauth2ResourceConfig.java
+++ server-demo1/src/main/java/dev/lowdad/cloud/serverdemo1/config/Oauth2ResourceConfig.java
@@ -1,11 +1,17 @@
 package dev.lowdad.cloud.serverdemo1.config;
 
-import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 /**
  * <p>
@@ -17,7 +23,30 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
  */
 @Configuration
 @EnableResourceServer
+@RefreshScope
 public class Oauth2ResourceConfig extends ResourceServerConfigurerAdapter {
+
+    private final JwtSignConfiguration jwtSignConfiguration;
+
+    @Value("${security.oauth2.resource.id}")
+    private String resourceId;
+
+    @Autowired
+    public Oauth2ResourceConfig(JwtSignConfiguration jwtSignConfiguration) {
+        this.jwtSignConfiguration = jwtSignConfiguration;
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(jwtTokenEnhancer());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtTokenEnhancer(){
+        JwtAccessTokenConverter jwtTokenEnhancer = new JwtAccessTokenConverter();
+        jwtTokenEnhancer.setSigningKey(jwtSignConfiguration.getSignKey());
+        return jwtTokenEnhancer;
+    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
@@ -26,5 +55,11 @@ public class Oauth2ResourceConfig extends ResourceServerConfigurerAdapter {
                 .and()
                 .csrf().disable();
 
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.resourceId(resourceId)
+                .tokenStore(tokenStore());
     }
 }
