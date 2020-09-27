02322e0702a6269f59ebd61d2359ec586accb4dc
Chongyu <javawcy@outlook.com>
2020-09-27 18:58:37 +0800

    feat: customer granter

--- server-demo1/src/main/java/dev/lowdad/cloud/serverdemo1/config/Oauth2ResourceConfig.java
+++ server-demo1/src/main/java/dev/lowdad/cloud/serverdemo1/config/Oauth2ResourceConfig.java
@@ -5,13 +5,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 /**
  * <p>
@@ -25,27 +30,56 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @EnableResourceServer
 public class Oauth2ResourceConfig extends ResourceServerConfigurerAdapter {
 
+    //定义token存储方式
+    private enum TokenStoreType {
+        REDIS, JWT
+    }
+
+    private final TokenStoreType tokenStoreType = TokenStoreType.JWT;
     private final JwtSignConfiguration jwtSignConfiguration;
+    private final RedisConnectionFactory redisConnectionFactory;
 
     @Value("${security.oauth2.resource.id}")
     private String resourceId;
 
     @Autowired
-    public Oauth2ResourceConfig(JwtSignConfiguration jwtSignConfiguration) {
+    public Oauth2ResourceConfig(JwtSignConfiguration jwtSignConfiguration, RedisConnectionFactory redisConnectionFactory) {
         this.jwtSignConfiguration = jwtSignConfiguration;
+        this.redisConnectionFactory = redisConnectionFactory;
     }
 
-    @Bean
-    public TokenStore tokenStore() {
-        return new JwtTokenStore(jwtTokenEnhancer());
-    }
-
+    /**
+     * jwt 序列化,signKey 可配置
+     *
+     * @return JwtAccessTokenConverter
+     */
     @Bean
     @RefreshScope
-    public JwtAccessTokenConverter jwtTokenEnhancer(){
+    public JwtAccessTokenConverter jwtTokenEnhancer() {
         JwtAccessTokenConverter jwtTokenEnhancer = new JwtAccessTokenConverter();
         jwtTokenEnhancer.setSigningKey(jwtSignConfiguration.getSignKey());
         return jwtTokenEnhancer;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * 定义token存储方式
+     *
+     * @return TokenStore
+     */
+    @Bean
+    public TokenStore tokenStore() {
+        switch (tokenStoreType) {
+            case REDIS:
+                return new RedisTokenStore(redisConnectionFactory);
+            case JWT:
+                return new JwtTokenStore(jwtTokenEnhancer());
+        }
+        return new InMemoryTokenStore();
     }
 
     @Override
