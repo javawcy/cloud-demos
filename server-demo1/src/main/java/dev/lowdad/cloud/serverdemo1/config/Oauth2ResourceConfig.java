27061bf4c16042a39330c6b881e09dba7bca9bd7
Chongyu <javawcy@outlook.com>
2020-09-28 09:56:36 +0800

    feat: add common

--- server-demo1/src/main/java/dev/lowdad/cloud/serverdemo1/config/Oauth2ResourceConfig.java
+++ server-demo1/src/main/java/dev/lowdad/cloud/serverdemo1/config/Oauth2ResourceConfig.java
@@ -1,22 +1,34 @@
 package dev.lowdad.cloud.serverdemo1.config;
 
+import dev.lowdad.cloud.common.model.vo.UserInfoVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cglib.beans.BeanMap;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.DefaultUserAuthenticationConverter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+import org.springframework.util.StringUtils;
+
+import java.util.Collection;
+import java.util.Map;
 
 /**
  * <p>
@@ -58,6 +70,20 @@ public class Oauth2ResourceConfig extends ResourceServerConfigurerAdapter {
     public JwtAccessTokenConverter jwtTokenEnhancer() {
         JwtAccessTokenConverter jwtTokenEnhancer = new JwtAccessTokenConverter();
         jwtTokenEnhancer.setSigningKey(jwtSignConfiguration.getSignKey());
+        ((DefaultAccessTokenConverter) jwtTokenEnhancer.getAccessTokenConverter()).setUserTokenConverter(new DefaultUserAuthenticationConverter() {
+            @Override
+            public Authentication extractAuthentication(Map<String, ?> map) {
+                UserInfoVO userInfoVO = new UserInfoVO();
+                BeanMap.create(userInfoVO).putAll(map);
+                Object authorities = map.get("authorities");
+                if (authorities instanceof String) {
+                    userInfoVO.setAuthorities(AuthorityUtils.commaSeparatedStringToAuthorityList((String) authorities));
+                } else if (authorities instanceof Collection) {
+                    userInfoVO.setAuthorities(AuthorityUtils.commaSeparatedStringToAuthorityList(StringUtils.collectionToCommaDelimitedString((Collection) authorities)));
+                }
+                return new PreAuthenticatedAuthenticationToken(userInfoVO, null, userInfoVO.getAuthorities());
+            }
+        });
         return jwtTokenEnhancer;
     }
 
@@ -84,7 +110,8 @@ public class Oauth2ResourceConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http
+        http    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeRequests().anyRequest().authenticated()
                 .and()
                 .csrf().disable();
